824488874a4120094a887fdeb8a58b63ce94de30
clemsonwaterski <jcarter@g.clemson.edu>
2013-11-13 10:37:38 -0500

    Implemented basic structure.  Added MainActivity, RecordMeal, and Spinner functionality.

--- src/com/tigerstripestech/codeathon/MainActivity.java
+++ src/com/tigerstripestech/codeathon/MainActivity.java
@@ -1,8 +1,12 @@
 package com.tigerstripestech.codeathon;
 
+import com.tigerstripestech.codeathon.RecordMeal;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +15,22 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	public void onClickRecordMeal(View v) {
+		Intent intent = new Intent(this, RecordMeal.class);
+		startActivity(intent);
+	}
+	/*
+	public void onClickCalendar(View v) {
+		Intent intent = new Intent(this, Calendar.class);
+		startActivity(intent);
+	}
+	
+	public void onClickProfile(View v) {
+		Intent intent = new Intent(this, Profile.class);
+		startActivity(intent);
+	}
+	*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
