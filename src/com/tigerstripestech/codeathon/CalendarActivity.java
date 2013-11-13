fb78e08de659483cb005728889df8cb6210e2674
Jason Carter <jccarte@gmail.com>
2013-11-13 15:26:38 -0500

    calories total working

--- src/com/tigerstripestech/codeathon/CalendarActivity.java
+++ src/com/tigerstripestech/codeathon/CalendarActivity.java
@@ -1,32 +1,23 @@
 package com.tigerstripestech.codeathon;
 
-import android.app.ActionBar.LayoutParams;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.DragEvent;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnDragListener;
-import android.view.View.OnLongClickListener;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
+
+import com.tigerstripestech.codeathon.db.MealDbHelper;
 
 public class CalendarActivity extends Activity {
 	
-	private PopupWindow mpopup;
 	SimpleDateFormat format = new SimpleDateFormat(
 			"EEE, d MMM yyyy");
 
@@ -36,6 +27,7 @@ public class CalendarActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         CalendarView myCal = (CalendarView) findViewById(R.id.cal);
+        
         
         myCal.setOnDateChangeListener(new OnDateChangeListener() {
 
@@ -52,9 +44,9 @@ public class CalendarActivity extends Activity {
             	focusTvDate.setText(format.format(date.getTime()));
             	
             	//Get the date to send as a parameter
-            	final int finalDate = (int)date.getTimeInMillis()/1000;
+            	final int finalDate = (int)(date.getTimeInMillis()/1000);
             	
-            	focusTvCalories.setText(getCalories(year, month, dayOfMonth));
+            	focusTvCalories.setText(getCalories(finalDate));
             	
                 Button btnDetails = (Button) findViewById(R.id.focusDetailsBtn);
                 btnDetails.setOnClickListener(new OnClickListener() 
@@ -62,13 +54,11 @@ public class CalendarActivity extends Activity {
                     @Override
                     public void onClick(View v) 
                     {
-                    	System.out.println(finalDate);
                     	
                     	Intent intent = new Intent(CalendarActivity.this, IntakeSelect.class);
                    
                    		Bundle extras = new Bundle();
                    		extras.putInt("date", finalDate);
-                   		
 
                    		intent.putExtras(extras);
                    		startActivity(intent);
@@ -82,6 +72,8 @@ public class CalendarActivity extends Activity {
             }
         });
         setupActionBar();
+        myCal.setDate(Calendar.getInstance().getTimeInMillis() - 86400000);
+        myCal.setDate(Calendar.getInstance().getTimeInMillis());
     }
     
     /**
@@ -94,8 +86,10 @@ public class CalendarActivity extends Activity {
 	}
 	
 	
-    private String getCalories(int year, int month,
-            int dayOfMonth){
-    	return "Custom Text!!";
+    private String getCalories(int date){
+    	MealDbHelper dbhelper = App.getDbHelper();
+    	int calories = dbhelper.getCaloriesFromDay(date);
+    	
+    	return ("Total Calories: " + calories);
     }
 }
